76f4c3dbdb33cd6ac3d7ac5a9ac15c5c2c054b73
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-12 23:14:02 +0700

    Update arithmetic.c

--- T03D03-1-develop/src/arithmetic.c
+++ T03D03-1-develop/src/arithmetic.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #define DIVBYZERO -99999
+#define eps 1e-6
 
 int sum(int a, int b);
 int diff(int a, int b);
 int mult(int a, int b);
 int div(int a, int b);
+
+int max(int a, int b);
+double important_function(float x);
+double fun();
 
 int main() {
   int a = 0;
@@ -18,8 +23,14 @@ int main() {
       printf("%d %d %d %d", sum(a, b), diff(a, b), mult(a, b), d);
   } else
     printf("n/a");
+
+  
+  double res = fun();
+  if (res <= eps)
+    printf("OK!");
   return 0;
 }
+
 int sum(int a, int b) { return a + b; }
 int diff(int a, int b) { return a - b; }
 int mult(int a, int b) { return a * b; }
@@ -29,3 +40,13 @@ int div(int a, int b) {
   }
   return a / b;
 }
+
+int max(int a, int b) { return a > b ? a : b; }
+
+double important_function(float x) {
+  return 7e-3 * pow(x, 4) +
+         ((22.8 * pow(x, 1. / 3.) - 1e3) * x + 3.) / (x * x / 2.) -
+         x * pow(10. + x, 2. / x) - 1.01;
+}
+
+double fun() { return (1.0 / 13) * (pow(((2 - 1.0) / (2 + 1.0)), 20)); }
