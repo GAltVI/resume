30b788950628559b01b8a72729ace8587bd9d8e4
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-13 00:02:23 +0700

    Update arithmetic.c

--- T03D03-1-develop/src/arithmetic.c
+++ T03D03-1-develop/src/arithmetic.c
@@ -10,33 +10,30 @@ int mult(int a, int b);
 int div(int a, int b);
 
 int max(int a, int b);
-double important_function(float x);
-double fun();
 void in_curcle(float x, float y);
 
 int main() {
   int a = 0;
   int b = 0;
+  printf("input: a, b = ");
   if (scanf("%d %d", &a, &b) == 2) {
-    int d = div(a, b);
-    if (d == DIVBYZERO)
-      printf("%d %d %d n/a", sum(a, b), diff(a, b), mult(a, b));
-    else
-      printf("%d %d %d %d", sum(a, b), diff(a, b), mult(a, b), d);
+      printf("max(a, b) = %d\n", max(a, b));
+      int d = div(a, b);
+      if (d == DIVBYZERO)
+      printf("a + b = %d | a - b = %d | a * b = %d | div by zero\n", sum(a, b), diff(a, b), mult(a, b));
+      else
+      printf("a + b = %d | a - b = %d | a * b = %d | a / b = %d\n", sum(a, b), diff(a, b), mult(a, b), d);
   } else
-    printf("n/a");
+    printf("@n/a");
 
-  
-  double res = fun();
-  if (res <= eps)
-    printf("OK!");
-
-  
   float x, y;
-  if (scanf("%f %f", &x, &y) == 2)
-    in_curcle(x, y);
+  printf("input: (x, y) = ");
+  if (scanf("%f %f", &x, &y) == 2) {
+      printf("Is point (%.2e, %.2e) in the circle x * x + y * y < 25: ", x, y);
+      in_curcle(x, y);
+  }
   else
-    printf("n/a");
+    printf("#n/a");
   return 0;
 }
 
@@ -51,14 +48,6 @@ int div(int a, int b) {
 }
 
 int max(int a, int b) { return a > b ? a : b; }
-
-double important_function(float x) {
-  return 7e-3 * pow(x, 4) +
-         ((22.8 * pow(x, 1. / 3.) - 1e3) * x + 3.) / (x * x / 2.) -
-         x * pow(10. + x, 2. / x) - 1.01;
-}
-
-double fun() { return (1.0 / 13) * (pow(((2 - 1.0) / (2 + 1.0)), 20)); }
 
 void in_curcle(float x, float y) {
   float f = x * x + y * y;
