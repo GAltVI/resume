6047164b2daf4fd247337e32cac2c844c6c453dd
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-13 19:09:28 +0700

    Create char_decode.h

---
+++ T04D04-1-develop/src/char_decode.h
@@ -0,0 +1,13 @@
+#ifndef H_MAX_PRIME_DEVISOR
+#define H_MAX_PRIME_DEVISOR
+
+#define NL '\n'
+#define SP ' '
+
+int code();
+int check_input_in_decode(char ch1, char ch2, char ch3);
+int check_hex(char ch);
+char upper_case(char ch);
+int decode();
+
+#endif
