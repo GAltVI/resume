f06ec9d6aa28d469ef48d58c25bfbc74ce9dadef
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-14 19:00:12 +0700

    Create search.h

---
+++ T05D08-1-develop/src/search.h
@@ -0,0 +1,18 @@
+#ifndef H_SEARCH
+#define H_SEARCH
+
+#define NMAX 30
+#define INTMAX 2147483647
+
+int input(int *a, int *n);
+void output(int *a, int n);
+int max(int *a, int n);
+int min(int *a, int n);
+long double mean(int *a, int n);
+long double variance(int *a, int n);
+void squaring(int *asqr, int *a, int n);
+void search(int *a, int n, long double mean_v, long double variance_v);
+int even(int x);
+int sigma_rule(int x, long double mean_v, long double variance_v);
+
+#endif
