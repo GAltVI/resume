1beaa17878360f5dc7eb809298f1ea62bc917af9
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-14 19:05:51 +0700

    Update search.h

--- T05D08-1-develop/src/search.h
+++ T05D08-1-develop/src/search.h
@@ -8,11 +8,11 @@ int input(int *a, int *n);
 void output(int *a, int n);
 int max(int *a, int n);
 int min(int *a, int n);
-long double mean(int *a, int n);
-long double variance(int *a, int n);
+double mean(int *a, int n);
+double variance(int *a, int n);
 void squaring(int *asqr, int *a, int n);
-void search(int *a, int n, long double mean_v, long double variance_v);
+void search(int *a, int n, double mean_v, double variance_v);
 int even(int x);
-int sigma_rule(int x, long double mean_v, long double variance_v);
+int sigma_rule(int x, double mean_v, double variance_v);
 
 #endif
