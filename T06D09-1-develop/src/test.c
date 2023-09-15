b875b6bf19a0fca4fe4f502e153f540e82647c4f
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-16 02:47:59 +0700

    Update test.c

--- T06D09-1-develop/src/test.c
+++ T06D09-1-develop/src/test.c
@@ -2,15 +2,18 @@
 #include "sort.h"
 #include "bignum.h"
 #include "several_arrays.h"
+#include "cycle_shift.h"
 
 void test_sort();
 void test_bignum();
 void test_several_arrays();
+void test_cycle_shift();
 
 int main() {
     test_sort();
     test_bignum();
     test_several_arrays();
+    test_cycle_shift();
     return 0;
 }
 
@@ -64,11 +67,13 @@ void test_several_arrays() {
         int counter = 0;
         int number = sum_numbers(data, n, &counter);
         if (counter) {
-            printf("%d\n", number);
+            printf("\n---------------------\nsum of odd elements: %d\n", number);
             int numbers[NMAX];
             int numbersLength = find_numbers(data, n, number, numbers);
-            if (numbersLength)
+            if (numbersLength) {
+                printf("new array: ");
                 output(numbers, numbersLength);
+            }
             else
                 printf("n/a");
         } else
@@ -77,3 +82,17 @@ void test_several_arrays() {
         printf("n/a");
 }
 
+void test_cycle_shift() {
+    printf("\n===\ntest cycle shift===\n");
+    int data[NMAX];
+    int n = 0;
+    int c = 0;
+    if (!input(data, &n, &c)) {
+        if (cycle_shift(data, n, c) == 0)
+            printf("\n---------------------\nresult: ");
+            output(data, n);
+        else
+            printf("n/a");
+    } else
+        printf("n/a");
+}
