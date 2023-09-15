5056e99a9afdf3ce846f0b964cd0912236cfdedc
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-16 03:00:15 +0700

    Update several_arrays.c

--- T06D09-1-develop/src/several_arrays.c
+++ T06D09-1-develop/src/several_arrays.c
@@ -1,26 +1,6 @@
 #include <stdio.h>
 #include "several_arrays.h"
 
-int input(int *buffer, int *length) {
-    int result = 0;
-    char ch;
-    if (scanf("%d%c", length, &ch) == 2 && *length > 0 && *length <= NMAX && ch == '\n') {
-        int *p = buffer;
-        for (; p - buffer < *length - 1 && !result; p++) {
-            if (scanf("%d%c", p, &ch) != 2 || ch != ' ') result = 1;
-        }
-        if (scanf("%d%c", p, &ch) != 2 || ch != '\n') result = 1;
-    } else
-        result = 1;
-    return result;
-}
-
-void output(int *buffer, int length) {
-    int *p = buffer;
-    for (; p - buffer < length - 1; p++) printf("%d ", *p);
-    printf("%d\n", *p);
-}
-
 int sum_numbers(int *buffer, int length, int *counter) {
     int sum = 0;
     *counter = 0;
