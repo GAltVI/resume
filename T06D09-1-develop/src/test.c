2d42b383e08ab256e64cbdac66df110d71e446d9
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-15 17:20:41 +0700

    Update test.c

--- T06D09-1-develop/src/test.c
+++ T06D09-1-develop/src/test.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include "sort.h"
 
+void test_sort();
+void test_bignum();
+
 int main() {
+    test_sort();
+    test_bignum();
+    return 0;
+}
+
+void test_sort() {
+    printf("\n===\ntest sort===\n");
     int data[N];
     int dataCopy[N];
     if (!input(data)) {
@@ -16,6 +26,28 @@ int main() {
         output(dataCopy, N);
     } else
         printf("n/a");
+}
 
-    return 0;
+void test_bignum() {
+    printf("\n===\ntest bignum===\n");
+    int len1, buff1[LEN];
+    int len2, buff2[LEN];
+    int sum_length, result_sum[LEN + 1];
+    int sub_length, result_sub[LEN];
+
+    if (!input(buff1, &len1) && !input(buff2, &len2)) {
+        delete_unsign_zero(buff1, &len1);
+        delete_unsign_zero(buff2, &len2);
+        printf("\n---------------------\nsum: ");
+        sum(buff1, len1, buff2, len2, result_sum, &sum_length);
+        output(result_sum, sum_length);
+
+        if (len1 >= len2) {
+            printf("\n---------------------\nsub: ");
+            sub(buff1, len1, buff2, len2, result_sub, &sub_length);
+            output(result_sub, sub_length);
+        } else
+            printf("n/a");
+    } else
+        printf("n/a");
 }
