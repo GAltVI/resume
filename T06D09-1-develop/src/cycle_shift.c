4a6599b1a8b687d3241c7ae6b4ac0ec313883421
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-16 02:58:50 +0700

    Update cycle_shift.c

--- T06D09-1-develop/src/cycle_shift.c
+++ T06D09-1-develop/src/cycle_shift.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 #include "cycle_shift.h"
+#include "io_array.h"
 
-int input(int *buffer, int *length, int *c) {
-    int result = 0;
-    char ch;
-    if (scanf("%d%c", length, &ch) == 2 && *length > 0 && *length <= NMAX && ch == '\n') {
-        int *p = buffer;
-        for (; p - buffer < *length - 1 && !result; p++) {
-            if (scanf("%d%c", p, &ch) != 2 || ch != ' ') result = 1;
-        }
-        if (scanf("%d%c", p, &ch) != 2 || ch != '\n') result = 1;
+int input_shift(int *buffer, int *length, int *c) {
+    if (!input(int *buffer, int *length))
         if (scanf("%d%c", c, &ch) != 2 || ch != '\n') result = 1;
     } else
         result = 1;
     return result;
-}
-
-void output(int *buffer, int length) {
-    int *p = buffer;
-    for (; p - buffer < length - 1; p++) printf("%d ", *p);
-    printf("%d\n", *p);
 }
 
 int cycle_shift(int *buffer, int length, int c) {
