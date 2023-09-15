2d489c4e68910aed7263b057a9998c5fb7c9239e
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-15 16:50:57 +0700

    Update test.c

--- T05D08-1-develop/src/test.c
+++ T05D08-1-develop/src/test.c
@@ -1,16 +1,16 @@
+#include <stdio.h>
 #include "search.h"
-#define NMAX 10
+//#define NMAX 10
 
 int main() {
     int n;
     int data[NMAX];
     if (!input(data, &n)) {
-     int max_v = max(data, n);
-      int min_v = min(data, n);
-      double mean_v = mean(data, n);
-      double variance_v = variance(data, n);
-       printf("%d %d %.6f %.6f", max_v, min_v, mean_v, variance_v);
-        search(data, n, mean_v, variance_v);
+        int max_v = max(data, n);
+        int min_v = min(data, n);
+        double mean_v = mean(data, n);
+        double variance_v = variance(data, n);
+        printf("%d %d %.6f %.6f %d", max_v, min_v, mean_v, variance_v, search(data, n, mean_v, variance_v));
     } else
         printf("n/a");
 
