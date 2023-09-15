54f360a392aa0f66a6e99963193a3c60f9fd90e9
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-16 03:09:45 +0700

    Update bignum.h

--- T06D09-1-develop/src/bignum.h
+++ T06D09-1-develop/src/bignum.h
@@ -4,6 +4,8 @@
 #define LEN 100
 #define TEN 10
 
+int input_bignum(int *buffer, int *length);
+
 void sum(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
 void sum_by_shift(int *buffMAX, int lenMAX, int *buffMIN, int lenMIN, int *result, int *result_length);
 
