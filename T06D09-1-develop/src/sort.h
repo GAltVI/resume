6ce4fd78d0439526e0f9b43a7d8563334d9b4686
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-15 17:01:08 +0700

    Create sort.h

---
+++ T06D09-1-develop/src/sort.h
@@ -0,0 +1,14 @@
+#ifndef H_SORT
+#define H_SORT
+
+#define N 10
+
+int input(int *a);
+void output(int *a, int n);
+void copy_arr(int *a, int *aCopy, int n);
+void quick_sort(int *l, int *r);
+void pyramid_sort(int *a, int n);
+void heapify(int *a, int n);
+void swap(int *a, int *b);
+
+#endif
