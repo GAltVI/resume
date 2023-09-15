04beef35bbc42503042448cbe694c391560d7956
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-16 03:24:45 +0700

    Update bignum.h

--- T06D09-1-develop/src/bignum.h
+++ T06D09-1-develop/src/bignum.h
@@ -12,9 +12,6 @@ void sum_by_shift(int *buffMAX, int lenMAX, int *buffMIN, int lenMIN, int *resul
 void sub(int *buff1, int len1, int *buff2, int len2, int *result, int *result_length);
 void sub_by_shift(int *buffMAX, int lenMAX, int *buffMIN, int lenMIN, int *result, int *result_length);
 
-int cycle_shift(int *buffer, int length, int c);
-void reverse(int *buffer, int length);
-
 int get_lager_buffer(int len1, int len2);
 void delete_unsign_zero(int *buff, int *len);
 
