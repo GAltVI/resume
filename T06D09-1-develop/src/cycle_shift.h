ceb393edd1d2c5ce353f015d7e3bb9ca1a9625c6
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-18 01:03:05 +0700

    Update cycle_shift.h

--- T06D09-1-develop/src/cycle_shift.h
+++ T06D09-1-develop/src/cycle_shift.h
@@ -1,8 +1,6 @@
 #ifndef H_CYCLE_SHIFT
 #define H_CYCLE_SHIFT
 
-#define NMAX 10
-
 int input_shift(int *buffer, int *length, int *c);
 int cycle_shift(int *buffer, int length, int c);
 void reverse(int *buffer, int length);
