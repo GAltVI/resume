0683d4769b80f426a1d43358602bf61b76bc4c62
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-18 16:28:56 +0700

    Update matrix.c

--- T07D10-1-develop/src/matrix.c
+++ T07D10-1-develop/src/matrix.c
@@ -1,50 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define M 100
-#define N 100
-#define STATIC_ARRAY 1
-#define POINTERS_WITHIN_ONE_BUFFER 2
-#define POINTERS_TO_ARRAYS 3
-#define POINTERS_TO_SEGMENTS 4
-#define NA "n/a"
-
-int static_array(int m, int n);
-int pointer_array_within_one_buffer(int m, int n);
-int pointer_array_of_arrays(int m, int n);
-int pointer_array_of_segments(int m, int n);
-
-void output(int **matrix, int m, int n);
-
-int main() {
-    int mode = 0;
-    int m, n;
-    if (scanf("%d", &mode) == 1 && mode > 0 && mode <= 4) {
-        if (scanf("%d %d", &m, &n) == 2 && n > 0 && n <= N && m > 0 && m <= M) {
-            int result = 0;
-            switch (mode) {
-                case STATIC_ARRAY:
-                    result = static_array(m, n);
-                    break;
-                case POINTERS_TO_SEGMENTS:
-                    result = pointer_array_of_segments(m, n);
-                    break;
-                case POINTERS_WITHIN_ONE_BUFFER:
-                    result = pointer_array_within_one_buffer(m, n);
-                    break;
-                case POINTERS_TO_ARRAYS:
-                    result = pointer_array_of_arrays(m, n);
-                    break;
-                default:
-                    printf(NA);
-                    break;
-            }
-            if (result == 1) printf(NA);
-        } else
-            printf(NA);
-    } else
-        printf(NA);
-    return 0;
-}
+#include "matrix.h"
 
 int static_array(int m, int n) {
     int result = 0;
