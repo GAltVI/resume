f6934e3ed2768403b801e41a9dc79ec5098a23cb
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-18 16:27:43 +0700

    Create matrix.h

---
+++ T07D10-1-develop/src/matrix.h
@@ -0,0 +1,19 @@
+#ifndef H_MATRIX
+#define H_MATRIX
+
+#define M 100
+#define N 100
+#define STATIC_ARRAY 1
+#define POINTERS_WITHIN_ONE_BUFFER 2
+#define POINTERS_TO_ARRAYS 3
+#define POINTERS_TO_SEGMENTS 4
+#define NA "n/a"
+
+int static_array(int m, int n);
+int pointer_array_within_one_buffer(int m, int n);
+int pointer_array_of_arrays(int m, int n);
+int pointer_array_of_segments(int m, int n);
+
+void output(int **matrix, int m, int n);
+
+#endif
