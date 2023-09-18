f5ba1ef4e6f113cd02936eed45ce3e68a7f51192
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-18 16:48:43 +0700

    Update matrix.h

--- T07D10-1-develop/src/matrix.h
+++ T07D10-1-develop/src/matrix.h
@@ -9,6 +9,7 @@
 #define POINTERS_TO_SEGMENTS 4
 #define NA "n/a"
 
+void minmax(int **matrix, int m, int n, int *min, int *max);
 int static_array(int m, int n);
 int pointer_array_within_one_buffer(int m, int n);
 int pointer_array_of_arrays(int m, int n);
