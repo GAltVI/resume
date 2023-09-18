5cfd986f5d7200f5b4feb7f8a53d4cafd09bf7e0
GAltVI <133541323+GAltVI@users.noreply.github.com>
2023-09-18 16:52:03 +0700

    Create matrix_extended.h

---
+++ T07D10-1-develop/src/matrix_extended.h
@@ -0,0 +1,20 @@
+#ifndef H_MATRIX_EXTENDED
+#define H_MATRIX_EXTENDED
+
+#define M 100
+#define N 100
+#define STATIC_ARRAY 1
+#define POINTERS_WITHIN_ONE_BUFFER 2
+#define POINTERS_TO_ARRAYS 3
+#define POINTERS_TO_SEGMENTS 4
+#define NA "n/a"
+
+void minmax(int **matrix, int m, int n, int *min, int *max);
+int static_array(int m, int n);
+int pointer_array_within_one_buffer(int m, int n);
+int pointer_array_of_arrays(int m, int n);
+int pointer_array_of_segments(int m, int n);
+
+void output(int **matrix, int m, int n);
+
+#endif
