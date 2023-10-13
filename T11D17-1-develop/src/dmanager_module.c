ed39a70a4b837e5a6e1381637cd76d31048d8088
Алтынбекова Гулайым Жанатовна <altynbekova.2014@corp.nstu.ru>
2023-10-13 17:51:35 +0700

    struct

--- T11D17-1-develop/src/dmanager_module.c
+++ T11D17-1-develop/src/dmanager_module.c
@@ -4,32 +4,36 @@
 
 #include "door_struct.h"
 
-#define DOORS_COUNT 15
+// #define DOORS_COUNT 999
 #define MAX_ID_SEED 10000
 
-void initialize_doors(struct door* doors);
+void initialize_doors(struct door* doors, int doors_count);
 
 int main() {
-    struct door doors[DOORS_COUNT];
-
-    initialize_doors(doors);
-
-    set_status(doors, DOORS_COUNT, 0);
-    sort(doors, doors + DOORS_COUNT);
-    output(doors, DOORS_COUNT);
-
+    // struct door doors[DOORS_COUNT];
+    int doors_count = 0;
+    if (scanf("%d", &doors_count) == 1 && doors_count > 1) {
+        struct door * doors = malloc(doors_count * sizeof(struct door));
+    
+        initialize_doors(doors, doors_count);
+        output(doors, doors_count);
+        printf("\n\n");
+        set_status(doors, doors_count, 0);
+        sort(doors, doors + doors_count);
+        output(doors, doors_count);
+    
+        free(doors);
+    } else printf("ERROR: invalid input");
     return 0;
 }
 
 // Doors initialization function
-// ATTENTION!!!
-// DO NOT CHANGE!
-void initialize_doors(struct door* doors) {
+void initialize_doors(struct door* doors, int doors_count) {
     srand(time(0));
 
     int seed = rand() % MAX_ID_SEED;
-    for (int i = 0; i < DOORS_COUNT; i++) {
-        doors[i].id = (i + seed) % DOORS_COUNT;
+    for (int i = 0; i < doors_count; i++) {
+        doors[i].id = (i + seed) % doors_count;
         doors[i].status = rand() % 2;
     }
 }
